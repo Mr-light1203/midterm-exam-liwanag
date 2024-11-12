5b99f2e1b6ee10ce39caa9aa9f31ef4d2280e0e3
Mr-light1203 <crlliwanag@gmail.com>
2024-11-13 05:07:12 +0800

    Index-page design and functionality added

---
+++ root/functions.php
@@ -0,0 +1,76 @@
+<?php
+function getUsers() {
+    return [
+        "user1@email.com" => "password",
+        "user2@email.com" => "password",
+        "user3@email.com" => "password",
+        "user4@email.com" => "password",
+        "user5@email.com" => "password"
+    ];
+}
+
+function checkUserSessionIsActive() {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start(); 
+    }
+}
+function displayErrorsLogin($errors) {
+    if (empty($errors)) {
+        return ''; 
+    }
+    
+    $output = '
+    <div class="alert alert-danger alert-dismissible fade show mx-auto my-2" style="max-width: 600px; margin-bottom: 20px;" role="alert">
+        <strong>System Errors:</strong> Please correct the following errors.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <hr>
+        <ul>';
+
+    foreach ($errors as $error) {
+        $output .= '<li>' . htmlspecialchars($error) . '</li>';
+    }
+    $output .= '</ul></div>';
+
+    return $output;
+}
+function validateLoginCredentials($email, $password) {
+    $errorArray = [];
+    $users = getUsers(); 
+
+    if (empty($email)) {
+        $errorArray['email'] = 'Email Address is required!';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errorArray['email'] = 'Email Address is invalid!';
+    }
+
+    if (empty($password)) {
+        $errorArray['password'] = 'Password is required!';
+    }
+
+    if (empty($errorArray)) {
+        if (!checkLoginCredentials($email, $password, $users)) {
+            $errorArray['credentials'] = 'Incorrect email or password!';
+        }
+    }
+
+    return $errorArray;
+}
+function checkLoginCredentials($email, $password, $users) {
+    return isset($users[$email]) && $users[$email] === $password;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
